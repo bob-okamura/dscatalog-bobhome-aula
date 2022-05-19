b354d3d6498fde48731cbfef7168f2fb5f5283ea
Roberto Okamura <rob.okamura@gmail.com>
2022-05-19 16:39:01 -0300

    Find Category by id, Exception handling

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO tb_category(name) VALUES ('BOOKS');
-INSERT INTO tb_category(name) VALUES ('BOSTA');
-INSERT INTO tb_category(name) VALUES ('BUCETA');
+INSERT INTO tb_category(name) VALUES ('ELECTRONICS');
+INSERT INTO tb_category(name) VALUES ('COMPUTERS');
