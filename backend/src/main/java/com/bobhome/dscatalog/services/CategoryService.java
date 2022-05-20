6460bfcdb565fa0ba075f622b3f7988c4a8e5da1
Roberto Okamura <rob.okamura@gmail.com>
2022-05-20 10:15:33 -0300

    Update category

--- backend/src/main/java/com/bobhome/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/bobhome/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.bobhome.dscatalog.dto.CategoryDTO;
 import com.bobhome.dscatalog.entities.Category;
 import com.bobhome.dscatalog.repositories.CategoryRepository;
-import com.bobhome.dscatalog.services.exceptions.EntityNotFoundException;
+import com.bobhome.dscatalog.services.exceptions.ResourceNotFoundException;
  
 @Service
 public class CategoryService {
@@ -43,8 +45,20 @@ public class CategoryService {
 		
 	}
 	
-
-	/*@Transactional(readOnly = true)
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id não encontrado!" + id);
+		}
+	}
+	
+		/*@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();
 		
