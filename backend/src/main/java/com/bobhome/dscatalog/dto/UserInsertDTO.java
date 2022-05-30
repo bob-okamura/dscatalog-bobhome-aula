3f5f5b1eb3a769d73ecb2e591157e4d09e6436d8
Roberto Okamura <rob.okamura@gmail.com>
2022-05-30 16:18:10 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/bobhome/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/bobhome/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.bobhome.dscatalog.dto;
 
+import com.bobhome.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 	
---
+++ backend/src/main/java/com/bobhome/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,42 @@
+package com.bobhome.dscatalog.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bobhome.dscatalog.dto.UserInsertDTO;
+import com.bobhome.dscatalog.entities.User;
+import com.bobhome.dscatalog.repositories.UserRepository;
+import com.bobhome.dscatalog.resources.exceptions.FieldMessage;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public void initialize(UserInsertValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		User user = repository.findByEmail(dto.getEmail());
+		if(user != null) {
+			list.add(new FieldMessage("email", "This email already exists!"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
